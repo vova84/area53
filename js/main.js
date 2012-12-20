db2d3f7a6018bf424a4b7e14b033081742fb3f25
vova84 <vovagromyko84@mail.ru>
2012-12-20 18:51:33 +0400

    полностью завершено навигационное меню
    в меню сделан поиск в режиме responsive написаны соотв. скрипты

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
+(function($){
 
+	// убираем меню по клику в мобилке
+	$('.menu-switcher').click(function(e){
+		$(this).prev().slideDown();
+		$(this).css({'display':'none'});
+		$('.search').fadeIn();
+		$('.search').css({'margin-top':'0'});
+		$('.close-menu').show();
+		e.preventDefault();
+	});
+	$('.close-menu').click(function(e){
+		$('.mainmenu ul').slideUp();
+		$('.menu-switcher').css({'display':'block'});
+		$('.search').fadeOut();
+		
+		e.preventDefault();
+	});
+	// меняем текст в кнопке сабмит
+	$('.search input[type="submit"]').attr('value','поиск');
+
+	// скрываем и показываем поиск
+
+
+	$('.search-switcher').toggle(function() {
+		  $('.search').stop('true','true').fadeIn();
+		  $('.search').css({'z-index':'1'});
+		},function(){
+			$('.search').stop('true','true').fadeOut();
+			$('.search').css({'z-index':'-1'});
+		});
+
+
+
+
+})(jQuery);
